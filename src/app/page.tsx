054e629c73d5175dea05b8bc369b11bd018e2c1b
Craig J. Midwinter <craig.j.midwinter@gmail.com>
2023-10-13 20:17:09 -0500

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,16 +11,12 @@ export default function Home() {
                     height={400}
                 />
             </div>
-            <h1 className="text-4xl font-bold">Game over</h1>
-            <h2 className="text-2xl font-bold">Thank you for your support!</h2>
-            <p className="text-lg text-gray-500 mt-2">We would like to inform you that the Goalfeed service has been
-                discontinued. Unfortunately, we are no longer able to provide the service moving forward. Rest assured,
-                we have refunded existing customers for the final month of service to ensure a fair resolution.</p>
-            <p className="text-lg text-gray-500 mt-2"> If you have any issues or questions regarding the discontinuation
-                or any other concerns, please feel free to reach out to us. You can contact us via email at
-                admin@goalfeed.com</p>
-            <p className="text-lg text-gray-500 mt-2">We apologize for any inconvenience this may cause and appreciate
-                your understanding.</p>
+            <h1 className="text-4xl font-bold">Goalfeed is Evolving!</h1>
+            <h2 className="text-2xl font-bold">Introducing the Open Source Replacement</h2>
+            <p className="text-lg text-gray-500 mt-2">We&apos;re excited to announce that while the old Goalfeed service has been discontinued, a free open source drop-in replacement has been created to take its place. This new service is available as a Go application, which you can find at <a href="https://github.com/goalfeed/goalfeed" target="_blank" rel="noopener noreferrer">goalfeed on GitHub</a>. For those using Home Assistant, there&apos;s also a hassio addon available at <a href="https://github.com/goalfeed/hassio-goalfeed-repository" target="_blank" rel="noopener noreferrer">hassio-goalfeed-repository on GitHub</a>.</p>
+            <p className="text-lg text-gray-500 mt-2">All your previous Home Assistant goalfeed automations should work seamlessly with this new service. We encourage contributors to join in and help improve the project!</p>
+            <p className="text-lg text-gray-500 mt-2">If you encounter any bugs or have feature requests, we encourage you to <a href="https://github.com/goalfeed/goalfeed/issues" target="_blank" rel="noopener noreferrer">open an issue on GitHub</a>. Alternatively, you can also email us directly at admin@goalfeed.com.</p>
+            <p className="text-lg text-gray-500 mt-2">Thank you for your continued support and understanding. We&apos;re excited for this new chapter and hope you are too!</p>
         </main>
     );
 }
